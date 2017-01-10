871a8d88fd7ffdf70e253388fb74d2f31ffb269b
bark10 <christian@metaboks.org>
2017-01-10 20:41:51 +0100

    Added licensing info

--- init.lua
+++ init.lua
@@ -1,3 +1,25 @@
+--[[
+Copyright (C) 2017 bark
+
+License for Code
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+License for Textures, Models and Sounds
+Gold and bark: CC-BY-SA 4.0
+--]]
+
 -- Make obsidian drop coins
 minetest.register_node(":default:obsidian", {
 	description = "Obsidian",
